5c3d528b9cda36f63ae5b89f209aeb80e4397b63
tongri <antohagryb@gmail.com>
2020-10-17 14:23:15 +0300

    big baby tape

--- netflix_clone/netflix_clone/movies/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Movie
-
-# Create your views here.
-def home(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/index.html', {
-        'movies': movies
-    })

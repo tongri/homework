fe0b3e898fb08ca13a31c9d5c6c01cc73b9ddf60
tongri <antohagryb@gmail.com>
2020-12-03 00:51:51 +0200

    one_more_try

--- 20/script.js
+++ 20/script.js
@@ -11,7 +11,7 @@ const search = (input) =>{
 }
 
 let button = document.getElementById('search');
-const body = document.getElementById('body');
+const body = document.querySelectorAll('body');
 
 button.addEventListener('click', () => {
 	let input = document.getElementById('film').value;
@@ -25,14 +25,18 @@ button.addEventListener('click', () => {
 			console.log(items);
 			for (let i = 0; i < items.length; i++){
 				let div_f_film = document.createElement("div");
-				body.appendChild(div_f_film);
+				body[0].appendChild(div_f_film);
 				div_f_film.className = 'single';
 				img = document.createElement("img");
 				h3 = document.createElement("h3");
 				h3.innerText = items[`${i}`]['show']['name'];
 				div_f_film.appendChild(h3);
-				div_f_film.appendChild(img);
-				img.src = items[`${i}`]['show']['image']['original'];
+				if (items[`${i}`]['show']['image'] !== null){
+					img = document.createElement("img");
+					div_f_film.appendChild(img);
+					img.src = items[`${i}`]['show']['image']['original'];
+					div_f_film.style.height = "550px";
+				}
 			}
 		})
 });
